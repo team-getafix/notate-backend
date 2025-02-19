43c929bb990ecf01c609abeed030b4cbbefc7fed
glomdom <22115@uktc-bg.com>
2025-02-19 18:23:40 +0200

    feat: remove aggregated swagger

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,17 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-import swaggerUi from "swagger-ui-express";
 import { createProxyMiddleware } from "http-proxy-middleware";
-import { aggregateSwaggerSpec } from "./swagger-aggregator";
+
+const bodyReconstructor = (proxyReq: any, req: any, res: any) => {
+  if (req.body) {
+    const bodyData = JSON.stringify(req.body);
+    proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+    proxyReq.write(bodyData);
+  }
+
+  console.log(`body: ${req.body}`)
+};
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth", createProxyMiddleware({
-  target: "http://auth-service:4001", changeOrigin: true, pathRewrite: (path, req) => {
-    return `/auth${path}`
+const authProxy = createProxyMiddleware({
+  target: 'http://auth-service:4001',
+  changeOrigin: true,
+  pathRewrite: (path: string) => `/auth${path}`,
+  on: {
+    proxyReq: bodyReconstructor
   },
-}));
+});
+
+app.use("/api/auth", authProxy);
 
 export default app;
