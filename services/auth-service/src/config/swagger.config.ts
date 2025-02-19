eb2c8762a34ca742cccd22c92576e03d4ebc1ea1
glomdom <22115@uktc-bg.com>
2025-02-19 18:01:10 +0200

    feat: use tsconfig

--- services/auth-service/src/config/swagger.config.ts
+++ services/auth-service/src/config/swagger.config.ts
@@ -1,6 +1,6 @@
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import { Express } from "express";
+import { type Express } from "express";
 
 const swaggerOptions = {
   definition: {
--- services/auth-service/src/middlewares/jwt.middleware.ts
+++ services/auth-service/src/middlewares/jwt.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 const SECRET_KEY = process.env.JWT_SECRET || "devsecret";
